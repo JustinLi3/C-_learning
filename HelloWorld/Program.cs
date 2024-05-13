6f154fb4bdb6feb47eb2aa976147f3e53613cdf5
Justin_Li <Triamethyest@gmail.com>
2024-05-13 15:04:27 -0400

    Practice with variables & constants.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,6 +10,27 @@ namespace HelloWorld //namespace is similar to package in Java
   {
     static void Main(string[] args) //similar to public static void main(String[] args)
     {
+      Console.WriteLine("Hello World"); //Just like System.out.print()  
+      //To run, do CTRL+F5 
+      const float Pi = 3.14f; //immutable kind of like final in Java, f at the end to tell compiler to treat as float, not double
+      //Practice with using basic data types
+      byte number = 2; //can use var instead, declare variable without specifying explicit type
+      int count = 10;
+      float totalPrice = 20.95f;
+      char character = 'A';
+      string firstName = "Justin";
+      bool isWorking = false;
+      Console.WriteLine(number); 
+      Console.WriteLine(count);
+      Console.WriteLine(totalPrice);
+      Console.WriteLine(character);
+      Console.WriteLine(firstName);
+      Console.WriteLine(isWorking);
+
+
+
+
+
     }
   }
 }
